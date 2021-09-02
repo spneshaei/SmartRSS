ad2bd7e37a6195d9229046afb8f8f38b991334e7
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-09-02 14:11:55 +0430

    Initial Commit

---
+++ Shared/SmartRSSApp.swift
@@ -0,0 +1,20 @@
+//
+//  SmartRSSApp.swift
+//  Shared
+//
+//  Created by Seyyed Parsa Neshaei on 9/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SmartRSSApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
